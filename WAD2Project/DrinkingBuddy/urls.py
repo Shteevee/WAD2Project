23dce84b506158e9a15eb5b98675db8ec9f1a0c3
Ewan Corbett <2195389c@student.gla.ac.uk>
2017-03-16 14:19:27 +0000

    Started work on slugs and context dictionary

--- WAD2Project/DrinkingBuddy/urls.py
+++ WAD2Project/DrinkingBuddy/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from DrinkingBuddy import views
+from DrinkingBuddy.models import Page
 
 app_name = 'DrinkingBuddy'
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     url(r'contactUs$', views.contactUs, name='contactUs'),
     url(r'barPages$', views.barPages, name='barPages'),
     url(r'signUp$', views.signUp, name='signUp'),
+    url(r'barPages/(?P<bar_name_slug>[\w\-]+)/$', views.bar, name='bar'),
     ]
--- WAD2Project/DrinkingBuddy/views.py
+++ WAD2Project/DrinkingBuddy/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from DrinkingBuddy.models import Page
 
 def index(request):
     return HttpResponse("Hello World!")
@@ -12,3 +13,15 @@ def barPages(request):
 
 def signUp(request):
     return HttpResponse("Sign Up!")
+
+def bar(request, bar_name_slug):
+    #context_dict = {}
+
+    #try:
+        #page = Page.objects.get(bar_name_slug)
+        #context_dict['page'] = page
+
+    #except Page.DoesNotExist:
+        #context_dict['page'] = None
+    #return HttpResponse(Page.objects.get(slug = bar_name_slug))
+    return HttpResponse("It worked!")
